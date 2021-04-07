d17e326637a6c5b19a74b2c1f9a6b70958ad3ff0
OlehFilyak <73895897+OlehFilyak@users.noreply.github.com>
2021-04-07 14:04:26 +0300

    Additing Remove Cloning DOM-nodes

--- workWithNodes.js
+++ workWithNodes.js
@@ -12,6 +12,33 @@ newImage.setAttribute(
 );
 newImage.setAttribute("alt", "dogs");
 
-console.log(newImage);
+// console.log(newImage);
 
 // Additing
+const Ul = document.querySelector("ul");
+// console.log(Ul); //<ul>
+//     <li><button class="add_new some_class second_new_class" name="add" data-index-number="777" data-author="Oleh" type="button" style="background-color: teal;">New Btn</button></li>
+//     <li><button class="remove" id="remove" type="button" name="remove">Удалить</button></li>
+//     <li><button class="clear" id="clear" type="button" name="clear" disabled="">Очистить</button></li>
+// </ul>
+
+// Ul.before(heading);
+// console.log(document);
+// Ul.replaceWith(newImage);
+// Ul.after(newImage);
+
+const anyBtn = document.createElement("li");
+anyBtn.innerHTML =
+  '<button type = "button" name="any_button">any Button</button>';
+Ul.appendChild(anyBtn); //additing to the end
+
+// Remove
+const btnClear = document.querySelector(".clear");
+// btnClear.remove();
+
+const firstLi = document.querySelector("li");
+// Ul.removeChild(firstLi);
+
+// Cloning
+const cloneUl = Ul.cloneNode(true);
+Ul.after(cloneUl);
