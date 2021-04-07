78b7bf9d98af36c6d80096e604996ebd5b2f57d3
OlehFilyak <73895897+OlehFilyak@users.noreply.github.com>
2021-04-07 13:14:03 +0300

    add attributes and dataAttributes

--- attributes_and_properties_by_DOM.js
+++ attributes_and_properties_by_DOM.js
@@ -8,8 +8,8 @@ btnAdd.textContent = "New Btn";
 btnAdd.style.backgroundColor = "teal";
 
 // classList
-const hasBtnAddClass = btnAdd.classList;
-console.log(hasBtnAddClass);
+const btnAddClassList = btnAdd.classList;
+// console.log(btnAddClassList);
 
 btnAdd.classList.add("new_class", "some_class", "second_new_class");
 btnAdd.classList.remove("new_class");
@@ -20,4 +20,15 @@ btnAdd.classList.toggle("add");
 btnAdd.classList.item(0); // повертає клас по індексу
 // console.log(btnAdd.classList.item(0));
 // console.log(btnAdd.classList.contains("add")); // повертає чи містить даний клас
-// console.log(btnAdd.classList.length);
+// console.log(btnAdd.classList.length); //розмір псевдомасиву
+
+// hasAttribute, getAttribute, setAttribute, removeAttribute, attributes
+// console.log(btnAdd.hasAttribute("id")); //true
+// console.log(btnAdd.getAttribute("id")); //add
+// btnAdd.setAttribute("type", "button"); // add type='button'
+// btnAdd.removeAttribute("id"); // remove id
+// console.log(btnAdd.attributes); //NamedNodeMap {0: class, 1: name, 2: style, class: class, name: name, style: style, length: 3}
+
+// dataAttributes
+// console.log(btnAdd.dataset.indexNumber); //777
+// console.log(btnAdd.dataset.author); //Oleh
